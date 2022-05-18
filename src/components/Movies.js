6f78ddf764e55811d57fe3fbde8f55fbb9a20e91
brhhae <mihriban.seydatar@cit.edu.al>
2022-05-18 17:29:53 +0200

    ADD: grab details data from database

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from "react-router-dom"
 import { selectMovies } from "../features/movie/movieSlice"
 import { useSelector } from "react-redux"
 
 
 function Movies() {
   const movies = useSelector(selectMovies);
-
-  console.log("This is movies" , movies);
   return (
     <Container>
         <h4>Recommended for You</h4>
@@ -15,7 +14,9 @@ function Movies() {
             { movies && 
                 movies.map((movie)=>(
                     <Wrap key={movie.id}>
+                        <Link to={`/detail/${movie.id}`} >
                         <img src={movie.cardImg} />
+                        </Link>
                     </Wrap>
                  ))
             }
